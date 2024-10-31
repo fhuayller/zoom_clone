dbd5bce31fd4c731618378b9cf01f31e24b66ccf
fhuayller <fhuayller@gmail.com>
2024-10-31 17:59:35 -0300

    navbar y sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} bg-dark-2 ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
